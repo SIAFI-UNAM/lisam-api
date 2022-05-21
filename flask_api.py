e9176843c5d0464094fb0604361af2ebddd6043d
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-05-21 00:44:36 -0500

    get-image-inference route name

--- flask_api.py
+++ flask_api.py
@@ -8,7 +8,7 @@ app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 imageInferenceController = ImageInferenceController()
 
 
-@app.route('/api/v1/get-image-inference/', methods=['POST'])
+@app.route('/api/v1/get-image-inference', methods=['POST'])
 def get_image_inference():
     req = request
 
