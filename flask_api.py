ebf65dde4257a0812f48080464100fcf9a79c04a
Erik Ivanov <erik.ivanov.n@gmail.com>
2022-07-04 01:50:09 -0500

    host default

--- flask_api.py
+++ flask_api.py
@@ -24,6 +24,6 @@ def index():
 
 if __name__ == '__main__':
     load_dotenv()
-    HOST = os.getenv('HOST')
+    HOST = os.environ.get("HOST", '0.0.0.0')
     port = int(os.environ.get("PORT", 5000))
     app.run(host=HOST, port=port)
